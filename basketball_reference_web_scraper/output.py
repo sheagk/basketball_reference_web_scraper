e5a1080630ae871518a74c0d0adbda9a6d9a58ae
Prajjwal <taoprajjwal@gmail.com>
2019-02-12 17:48:30 +0400

    Added advanced stats season totals. Changed output_type so that it accepts "CSV" as "JSON" as strings

--- basketball_reference_web_scraper/output.py
+++ basketball_reference_web_scraper/output.py
@@ -75,6 +75,35 @@ team_box_score_fieldname = [
     "personal_fouls",
 ]
 
+player_advanced_fieldname=[
+        "name",
+        "positions",
+        "age",
+        "team",
+        "games_played",
+        "minutes_played",
+        "player_efficiency_rating",
+        "true_shooting_percent",
+        "three_point_attempt_rate",
+        "free_throw_attempt_rate",
+        "offensive_rebound_pct",
+        "defensive_rebound_pct",
+        "total_rebound_pct",
+        "assists_pct",
+        "steals_pct",
+        "blocks_pct",
+        "turnover_pct",
+        "usage_pct",
+        "offensive_win_shares",
+        "defensive_win_shares",
+        "total_win_shares",
+        "offensive_bpm",
+        "defensive_bpm",
+        "total_bpm",
+        "vorp"
+
+]
+
 default_json_options = {
     "sort_keys": True,
     "indent": 4,
@@ -93,7 +122,7 @@ def output(values, output_type, output_file_path, encoder, csv_writer, output_wr
 
     write_option = OutputWriteOption.WRITE if output_write_option is None else output_write_option
 
-    if output_type == OutputType.JSON:
+    if output_type == OutputType.JSON or output_type=="JSON":
         options = default_json_options if json_options is None else merge_two_dicts(first=default_json_options, second=json_options)
         if output_file_path is None:
             return json.dumps(values, cls=encoder, **options)
@@ -101,7 +130,7 @@ def output(values, output_type, output_file_path, encoder, csv_writer, output_wr
             with open(output_file_path, write_option.value, newline="") as json_file:
                 return json.dump(values, json_file, cls=encoder, **options)
 
-    if output_type == OutputType.CSV:
+    if output_type == OutputType.CSV or output_type=="CSV":
         if output_file_path is None:
             raise ValueError("CSV output must contain a file path")
         else:
@@ -187,6 +216,40 @@ def players_season_totals_to_csv(rows, output_file_path, write_option):
             } for row in rows
         )
 
+
+def players_advanced_to_csv(rows,output_file_path,write_option):
+    with open(output_file_path, write_option.value, newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=player_advanced_fieldname)
+        writer.writeheader()
+        writer.writerows(
+            {
+                "name": row["name"],
+                "positions": "-".join(map(lambda position: position.value, row["positions"])),
+                "age": row["age"],
+                "team": row["team"].value,
+                "games_played": row["games_played"],
+                "minutes_played": row["minutes_played"],
+                "player_efficiency_rating": row["player_efficiency_rating"],
+                "true_shooting_percent": row["true_shooting_percent"],
+                "three_point_attempt_rate": row["three_point_attempt_rate"],
+                "free_throw_attempt_rate": row["free_throw_attempt_rate"],
+                "offensive_rebound_pct": row["offensive_rebound_pct"],
+                "defensive_rebound_pct": row["defensive_rebound_pct"],
+                "total_rebound_pct": row["total_rebound_pct"],
+                "assists_pct": row["assists_pct"],
+                "steals_pct": row["steals_pct"],
+                "blocks_pct":row["blocks_pct"],
+                "turnover_pct":row["turnover_pct"] ,
+                "usage_pct":row["usage_pct"],
+                "offensive_win_shares":row["offensive_win_shares"] ,
+                "defensive_win_shares":row["defensive_win_shares"] ,
+                "total_win_shares":row["total_win_shares"],
+                "offensive_bpm":row["offensive_bpm"],
+                "defensive_bpm":row["defensive_bpm"],
+                "total_bpm":row["total_bpm"],
+                "vorp":row["vorp"],
+            } for row in rows
+        )
 
 def team_box_scores_to_csv(rows, output_file_path, write_option):
     with open(output_file_path, write_option.value, newline="") as csv_file:
